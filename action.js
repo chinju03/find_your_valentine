8f5c7b63c825691c18a116e603fb511816e4e278
chinju03 <99011016+chinju03@users.noreply.github.com>
2022-02-04 19:26:52 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,13 @@
+function changeToViolet(){
+
+    document.body.style.backgroundImage = "url('https://media.istockphoto.com/photos/surreal-dark-chrome-pink-and-purple-flower-dahlia-macro-isolated-on-picture-id949472744?k=20&m=949472744&s=612x612&w=0&h=AU6rq7dvYllJYw99AE7-T5kQ6udCV2JZacosjKnysIo=')";
+    window.open("https://www.hinge.co/", "blank").focus();
+}
+function changeToTeal(){
+    document.body.style.backgroundImage = "url('https://media.istockphoto.com/photos/surreal-dark-chrome-turquoise-flower-dahlia-macro-isolated-picture-id514221132?k=20&m=514221132&s=612x612&w=0&h=JVETD8RsxpMWwjXxx3a0WF02iqidzT8CYKlwEyEDqGI=')";
+    window.open("https://tinder.com/", "blank").focus();
+}
+function changeToBlue(){
+    document.body.style.backgroundImage = "url('https://media.istockphoto.com/photos/surreal-dark-chrome-blue-flower-dahlia-macro-isolated-on-black-picture-id497144276?k=20&m=497144276&s=612x612&w=0&h=_AbPQWiIKe7f-rDS4Lo7h5oPFVwupbVL8iPXbSF8TaI=')";
+    window.open("https://bumble.com/en-in/", "blank").focus();
+}
